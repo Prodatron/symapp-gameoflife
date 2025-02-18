d6277dbe9c0bc2bb2097e5090704dce28fbeddcd
Prodatron <jmika@prodatron.net>
2025-02-18 13:25:23 +0100

    makefile updated

--- App-GameOfLife1.asm
+++ App-GameOfLife1.asm
@@ -2,5 +2,5 @@ nolist
 
 org #1000
 write "f:\symbos\apps\gamelife.exe"
-READ "..\..\..\..\SVN-Main\trunk\SymbOS-Constants.asm"
+READ "..\..\..\SRC-Main\SymbOS-Constants.asm"
 READ "App-GameOfLife.asm"
